d8180b4d4f42a410eb45af1ffcf6581a2e9a3c4a
Jeroen Mulder <jeroen.mulder.1989@gmail.com>
2017-06-13 15:10:03 +0200

    re-commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Opdracht 1 (relative positioning & selectors & CSS3)</title>
+    
+	<style type="text/css">
+		.a{
+			border: 1px solid green;
+			border-radius: 7px;
+			width:	1000px;
+		}
+			
+		.b{
+			border: 1px solid red;
+			border-radius: 5px;
+			min-width: 200px;
+			max-width: 400px;
+			float: left;	
+			text-align: center;
+		}
+		
+		.c{
+			border: 1px solid red;
+			border-radius: 5px;
+			min-width: 200px;
+			max-width: 400px; 
+			float: left;
+			background: green;
+			text-align: center;
+		}
+	</style>
+	
+	<link href="style.css" rel="stylesheet" type="text/css">
+	
+	<script type="text/javascript">
+		
+	</script>
+	
+</head>
+
+<body>
+	<div class="a">
+		<h1>hello there</h1>
+		<p>It's good to see you.</p>
+		<div class="b">hello</div>
+		<div class="c">there</div>
+		
+		</br><p>these are div's, neat right</p>
+	</div>
+</body>
+
